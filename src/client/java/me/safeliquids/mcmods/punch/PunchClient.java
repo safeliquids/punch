575dde4092401b2f09084ffb0d29b7b466ead249
safeliquids <neumetel.kovovy@gmail.com>
2024-12-16 20:28:51 +0100

    use ModifyExpressionValue instead of ModifyConstant to disable attack speed check

--- src/client/java/me/safeliquids/mcmods/punch/PunchClient.java
+++ src/client/java/me/safeliquids/mcmods/punch/PunchClient.java
@@ -5,15 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PunchClient implements ClientModInitializer {
-    private static boolean doModifyCrosshairRendering = true;
     private static final Logger LOGGER = LoggerFactory.getLogger("punch");
+
     @Override
     public void onInitializeClient() {
-        doModifyCrosshairRendering = true;
         LOGGER.info("loading punch mod");
-    }
-
-    public static boolean shouldModifyCrosshairRendering() {
-        return doModifyCrosshairRendering;
     }
 }
