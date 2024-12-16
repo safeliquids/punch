2b821b6894d047bc25459d261685065a69833dd1
safeliquids <neumetel.kovovy@gmail.com>
2024-12-17 00:13:22 +0100

    entirely bypass calling getAttackCooldownProgressPerTick

--- src/client/java/me/safeliquids/mcmods/punch/mixin/client/CrosshairRenderingMixin.java
+++ src/client/java/me/safeliquids/mcmods/punch/mixin/client/CrosshairRenderingMixin.java
@@ -1,41 +1,41 @@
 package me.safeliquids.mcmods.punch.mixin.client;
 
-import me.safeliquids.mcmods.punch.PunchClient;
-
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.network.ClientPlayerEntity;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(InGameHud.class)
 public abstract class CrosshairRenderingMixin {
     /**
      * Allows showing the "full crosshair" texture regardless of attack speed.
-     *
+     * <p>
      * The client renders the crosshair in a special way if
      * 1. the player is looking at a living entity,
      * 2. this entity is currently alive,
      * 3. the attack cooldown is fully recovered, and
      * 4. the player's current attack speed is less than 4.
-     *
+     * <p>
      * The last condition corresponds to checking if the time T (in ticks) it takes for the attack cooldown to fully
      * recover is more than 5. (NB that, in the yarn mappings, this time is queried using the incorrectly named method
-     * `getAttackCooldownProgressPerTick`.) This callback removes that last condition by replacing the constant with -1.
+     * `getAttackCooldownProgressPerTick`.) This callback removes that last condition by replacing the obtained time T
+     * with 100 (which is always more than 5).
      *
-     * @param original original value of T
-     * @return substitute value of T if rendering should be modified, or the original
+     * @param instance the player entity
+     * @param original something idk
+     * @return always 100.0f
      */
-    @ModifyConstant(
-        method="Lnet/minecraft/client/gui/hud/InGameHud;renderCrosshair(Lnet/minecraft/client/gui/DrawContext;)V",
-        constant=@Constant(floatValue=5.0f),
+    @WrapOperation(
+        method="renderCrosshair(Lnet/minecraft/client/gui/DrawContext;)V",
+        at=@At(
+            value="INVOKE",
+            target="Lnet/minecraft/client/network/ClientPlayerEntity;getAttackCooldownProgressPerTick()F"),
         allow=1,
         require=1)
-    private float inRenderCrosshair(float original) {
-        // as of now this method always returns true
-        if (PunchClient.shouldModifyCrosshairRendering()) {
-            return -1.0f;
-        }
-        return original;
+    private float onGetAttackCooldownProgressPerTick(ClientPlayerEntity instance, Operation<Float> original) {
+        return 100f;
     }
 }
