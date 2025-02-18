a4ae74706d471d827bf04d215d0197ee02f9a62c
safeliquids <neumetel.kovovy@gmail.com>
2025-02-18 19:17:31 +0100

    Implement custom attack indicator sizes.
    The crosshair attack indicator can have a custom size instead of the 16x4 pixels
    hard-coded in Vanilla. Optionally, the size can change when targeting an entity.
    
    Added a configuration interface for custom indicator sizes and showing the full
    indicator texture regardless of attack speed.

--- src/client/java/me/safeliquids/mcmods/punch/PunchClient.java
+++ src/client/java/me/safeliquids/mcmods/punch/PunchClient.java
@@ -1,24 +1,155 @@
 package me.safeliquids.mcmods.punch;
 
 import net.fabricmc.api.ClientModInitializer;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Files;
+
 public class PunchClient implements ClientModInitializer {
     private static final Logger LOGGER = LoggerFactory.getLogger("punch");
-    private static int sTargetAttackIndicatorWidth = 32;
-    private static int sTargetAtackIndicatorHeight = 8;
+    private static final Path CONFIG_FILE_PATH = Path.of(".", "config", "punch.json");
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private static Config sConfig = null;
 
     @Override
     public void onInitializeClient() {
         LOGGER.info("loading punch mod");
+        // pretty printing
+        MAPPER.enable(SerializationFeature.INDENT_OUTPUT);
+        sConfig = readConfig();
+        if (sConfig == null) {
+            sConfig = Config.makeDefault();
+            writeConfig(sConfig);
+        }
     }
 
-    public static int getTargetAttackIndicatorWidth() {
-        return sTargetAttackIndicatorWidth;
+    public static Config getConfig() {
+        return sConfig;
     }
 
-    public static int getTargetAttackIndicatorHeight() {
-        return sTargetAtackIndicatorHeight;
+    private Config readConfig() {
+        if (!canReadConfigFile()) {
+            LOGGER.info("Config file is not available");
+            return null;
+        }
+        Config config;
+        try {
+            config = MAPPER.readValue(CONFIG_FILE_PATH.toFile(), Config.class);
+        }
+        // jackson-databind documentation says IOException is raised when a low-level I/O issue occurs, but that is
+        // not true. IOException is actually raised even when the file is invalid JSON or when there is a problem
+        // with constructing the POJO (e.g. when the target class does not have a parameter-less constructor)
+        catch (IOException ex) {
+            LOGGER.warn("ioexception when reading config file");
+            config = null;
+        }
+
+        if (config == null) {
+            LOGGER.warn("Could not read config from file");
+            return null;
+        }
+        Config sanitizedConfig = config.sanitized();
+        if (config != sanitizedConfig) {
+            LOGGER.warn("Invalid entries in config file");
+        }
+        return sanitizedConfig;
+    }
+
+    private void writeConfig(Config config) {
+        if (config == null) {
+            return;
+        }
+        if (!Files.exists(CONFIG_FILE_PATH)) {
+            try {
+                Files.createDirectories(CONFIG_FILE_PATH.getParent());
+            }
+            catch (IOException ex) {
+                LOGGER.warn("could not create config directory");
+                return;
+            }
+        }
+        else if (!Files.isRegularFile(CONFIG_FILE_PATH) || !Files.isWritable(CONFIG_FILE_PATH)) {
+            LOGGER.warn("config file exists but is not writable");
+            return;
+        }
+        try {
+            MAPPER.writeValue(CONFIG_FILE_PATH.toFile(), config);
+        }
+        catch (IOException ex) {
+            LOGGER.warn("could not write config to file");
+        }
+    }
+
+    private boolean canReadConfigFile() {
+        return Files.exists(CONFIG_FILE_PATH)
+            && Files.isRegularFile(CONFIG_FILE_PATH) && Files.isReadable(CONFIG_FILE_PATH);
+    }
+
+    private boolean canWriteConfigFile() {
+        return Files.exists(CONFIG_FILE_PATH)
+            && Files.isRegularFile(CONFIG_FILE_PATH) && Files.isWritable(CONFIG_FILE_PATH);
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown=true)
+    public static class Config {
+        @JsonProperty("enabled")
+        public final boolean mEnabled;
+        @JsonProperty("show_indicator_with_fast_weapon")
+        public final boolean mShowIndicatorWithFastWeapon;
+        @JsonProperty("do_custom_indicator_sizes")
+        public final boolean mDoCustomIndicatorSizes;
+        @JsonProperty("indicator_width")
+        public final int mIndicatorWidth;
+        @JsonProperty("indicator_height")
+        public final int mIndicatorHeight;
+        @JsonProperty("targeted_indicator_width")
+        public final int mTargetedIndicatorWidth;
+        @JsonProperty("targeted_indicator_height")
+        public final int mTargetedIndicatorHeight;
+
+        public Config() {
+            mEnabled = true;
+            mShowIndicatorWithFastWeapon = true;
+            mDoCustomIndicatorSizes = true;
+            mIndicatorWidth = 16;
+            mIndicatorHeight = 4;
+            mTargetedIndicatorWidth = 32;
+            mTargetedIndicatorHeight = 8;
+        }
+
+        public Config(
+            boolean enabled, boolean fast_weapon, boolean custom_sizes, int width, int height, int t_width,
+            int t_height)
+        {
+            mEnabled = enabled;
+            mShowIndicatorWithFastWeapon = fast_weapon;
+            mDoCustomIndicatorSizes = custom_sizes;
+            mIndicatorWidth = width > 0 ? width : 1;
+            mIndicatorHeight = height > 0 ? height : 1;
+            mTargetedIndicatorWidth = t_width > 0 ? t_width : 1;
+            mTargetedIndicatorHeight = t_height > 0 ? t_height : 1;
+        }
+
+        public Config sanitized() {
+            if (mIndicatorWidth > 0
+                    && mIndicatorHeight > 0 && mTargetedIndicatorWidth > 0 && mTargetedIndicatorHeight > 0) {
+                return this;
+            }
+            return new Config(
+                mEnabled, mShowIndicatorWithFastWeapon, mDoCustomIndicatorSizes, mIndicatorWidth, mIndicatorHeight,
+                mTargetedIndicatorWidth, mTargetedIndicatorHeight);
+        }
+
+        public static Config makeDefault() {
+            return new Config(true, true, true, 16, 4, 32, 8);
+        }
     }
 }
--- src/client/java/me/safeliquids/mcmods/punch/mixin/client/CrosshairRenderingMixin.java
+++ src/client/java/me/safeliquids/mcmods/punch/mixin/client/CrosshairRenderingMixin.java
@@ -13,9 +13,25 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
 public abstract class CrosshairRenderingMixin {
+    private PunchClient.Config mConfig = null;
+
+    /**
+     * Caches current mod configuration.
+     */
+    @Inject(
+        method="renderCrosshair(Lnet/minecraft/client/gui/DrawContext;)V",
+        at=@At("HEAD"))
+    private void onRenderCrosshair(DrawContext context, CallbackInfo ci) {
+        PunchClient.Config newConfig = PunchClient.getConfig();
+        if (mConfig == null || mConfig != newConfig) {
+            mConfig = newConfig;
+        }
+    }
+
     /**
      * Allows showing the "full crosshair" texture regardless of attack speed.
      * <p>
@@ -42,9 +58,18 @@ public abstract class CrosshairRenderingMixin {
         allow=1,
         require=1)
     private float onGetAttackCooldownProgressPerTick(ClientPlayerEntity instance, Operation<Float> original) {
-        return 100f;
+        if (mConfig != null && mConfig.mShowIndicatorWithFastWeapon) {
+            return 100f;
+        }
+        return original.call(instance);
     }
 
+    /**
+     * Custom size of full attack indicator.
+     *
+     * If enabled, changes the size of the "full attack indicator" texture as it is drawn on the HUD. This texture is
+     * only drawn when an entity is targeted so the TargetedIndicatorWidth is always used.
+     */
     @WrapOperation(
         method="renderCrosshair(Lnet/minecraft/client/gui/DrawContext;)V",
         at=@At(
@@ -56,11 +81,26 @@ public abstract class CrosshairRenderingMixin {
     private void onDrawFullAttackIndicator(
         DrawContext instance, Identifier texture, int x, int y, int width,  int height, Operation<Void> original)
     {
-        final int w = PunchClient.getTargetAttackIndicatorWidth();
-        final int adjusted_x = x + 8 - (w / 2);
-        original.call(instance, texture, adjusted_x, y, w, w);
+        if (mConfig != null && mConfig.mEnabled && mConfig.mDoCustomIndicatorSizes) {
+            width = mConfig.mTargetedIndicatorWidth;
+            // this is not a mistake, the full indicator texture is a square
+            height = width;
+            // The x-position of the attack indicator is moved to the left of the center of the screen by half its
+            // width. This line removes the hard-coded offset in Minecraft's code (8 because the vanilla width is 16)
+            // and replaces it with a corresponding offset based on the custom width. (Here that is
+            // TargetedIndicatorWidth since the "full" texture is only shown when targeting something. In the other
+            // methods it is either the normal or targeted width.)
+            x = x + 8 - (width / 2);
+        }
+        original.call(instance, texture, x, y, width, height);
     }
 
+    /**
+     * Custom drawing of the background of the partial attack indicator.
+     *
+     * If enabled, changes the size of the partial attack indicator texture on the HUD, specifically its background.
+     * The size also depends on if an entity is targeted.
+     */
     @WrapOperation(
         method="renderCrosshair(Lnet/minecraft/client/gui/DrawContext;)V",
         at=@At(
@@ -72,15 +112,30 @@ public abstract class CrosshairRenderingMixin {
     private void onDrawAttackIndicatorBackground(
         DrawContext instance, Identifier texture, int x, int y, int width,  int height, Operation<Void> original)
     {
+        if (mConfig == null || !mConfig.mEnabled || !mConfig.mDoCustomIndicatorSizes) {
+            original.call(instance, texture, x, y, width, height);
+            return;
+        }
         final MinecraftClient client = ((IMinecraftClientAccessor) this).getClient();
         if (client.targetedEntity != null && client.targetedEntity instanceof LivingEntity) {
-            width = PunchClient.getTargetAttackIndicatorWidth();
-            height = PunchClient.getTargetAttackIndicatorHeight();
-            x = x + 8 - (width / 2);
+            width = mConfig.mTargetedIndicatorWidth;
+            height = mConfig.mTargetedIndicatorHeight;
         }
+        else {
+            width = mConfig.mIndicatorWidth;
+            height = mConfig.mIndicatorHeight;
+        }
+        x = x + 8 - (width / 2);
         original.call(instance, texture, x, y, width, height);
     }
 
+    /**
+     * Custom drawing of the partial attack indicator.
+     *
+     * If enabled, changes the size of the partial attack indicator texture on the HUD. The overall size depends on
+     * if an entity is targeted. The width of the part that is drawn relative to the width of the background also
+     * depends on the attack cooldown progress.
+     */
     @WrapOperation(
         method="renderCrosshair(Lnet/minecraft/client/gui/DrawContext;)V",
         at=@At(
@@ -90,20 +145,26 @@ public abstract class CrosshairRenderingMixin {
         require=1,
         allow=1)
     private void onDrawAttackIndicatorForeground(
-        DrawContext instance, Identifier texture, int i, int j, int k, int l, int x, int y, int width, int height,
-        Operation<Void> original)
+        DrawContext instance, Identifier texture, int texture_width, int texture_height, int start_u, int start_v,
+        int x, int y, int width, int height, Operation<Void> original)
     {
+        if (mConfig == null || !mConfig.mEnabled || !mConfig.mDoCustomIndicatorSizes) {
+            original.call(instance, texture, texture_width, texture_height, start_u, start_v, x, y, width, height);
+            return;
+        }
         final MinecraftClient client = ((IMinecraftClientAccessor) this).getClient();
         if (client.targetedEntity != null && client.targetedEntity instanceof LivingEntity) {
-            final int w = PunchClient.getTargetAttackIndicatorWidth();
-            final int h = PunchClient.getTargetAttackIndicatorHeight();
-            x = x + 8 - (w / 2);
-            i = w;
-            j = h;
-            final float f = client.player.getAttackCooldownProgress(0.0F);
-            width = (int) (f * w);
-            height = h;
+            texture_width = mConfig.mTargetedIndicatorWidth;
+            texture_height = mConfig.mTargetedIndicatorHeight;
         }
-        original.call(instance, texture, i, j, k, l, x, y, width, height);
+        else {
+            texture_width = mConfig.mIndicatorWidth;
+            texture_height = mConfig.mIndicatorHeight;
+        }
+        x = x + 8 - (texture_width / 2);
+        final float f = client.player.getAttackCooldownProgress(0.0F);
+        width = (int) (f * texture_width);
+        height = texture_height;
+        original.call(instance, texture, texture_width, texture_height, start_u, start_v, x, y, width, height);
     }
 }
